778e3512c082807975f7d6593b5fab16bbac5ddf
Leandro Asrilevich <beastx@gmail.com>
2010-10-21 10:12:16 -0300

    added common functions, and fix path for included files

--- includeBeastxWordpressTools.php
+++ includeBeastxWordpressTools.php
@@ -1,6 +1,18 @@
 <?
-require_once 'FirePHP.class.php';
+require_once 'thirdParty/firePHP/FirePHP.class.php';
+require_once 'class.BeastxInputs.php';
+require_once 'class.BeastxFileSystemHelper.php';
+require_once 'class.BeastxMysqlHelper.php';
+require_once 'class.BeastxOptionsManager.php';
 require_once 'class.BeastxPlugin.php';
 require_once 'class.BeastxAdminPage.php';
-require_once 'class.BeastxInputs.php';
+
+function debug($var, $title = null) {
+    $firephp = FirePHP::getInstance(true);
+    $firephp->log($var, $title);
+}
+
+if (defined(BEASTXDEBUG) && BEASTXDEBUG) {
+    $wpdb->show_errors = true;
+}
 ?>
