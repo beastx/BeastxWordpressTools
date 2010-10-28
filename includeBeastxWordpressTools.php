72119d6a18208f6f12cc41e8a7afb77476150fbd
Leandro Asrilevich <beastx@gmail.com>
2010-10-28 01:19:58 -0300

    dont re include FirePHP

--- includeBeastxWordpressTools.php
+++ includeBeastxWordpressTools.php
@@ -4,7 +4,14 @@ ob_start();
 
 if (!defined('ABSPATH')) die("Aren't you supposed to come here via WP-Admin?");
 
-require_once 'thirdParty/firePHP/FirePHP.class.php';
+if (!function_exists('debug')) {
+    require_once 'thirdParty/firePHP/FirePHP.class.php';
+    function debug($var, $title = null) {
+        $firephp = FirePHP::getInstance(true);
+        $firephp->log($var, $title);
+    }
+}
+
 require_once 'class.BeastxInputs.php';
 require_once 'class.BeastxEnviroment.php';
 require_once 'class.BeastxFileSystemHelper.php';
@@ -13,13 +20,6 @@ require_once 'class.BeastxOptionsManager.php';
 require_once 'class.BeastxPlugin.php';
 require_once 'class.BeastxAdminPage.php';
 require_once 'class.BeastxCustomPostType.php';
-
-if (!function_exists('debug')) {
-    function debug($var, $title = null) {
-        $firephp = FirePHP::getInstance(true);
-        $firephp->log($var, $title);
-    }
-}
 
 if (defined(BEASTXDEBUG) && BEASTXDEBUG) {
     $wpdb->show_errors = true;
